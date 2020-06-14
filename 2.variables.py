fee5c9247872136cc132e4ddde4e6907aebe12a3
YogeshDeveloper <spyogesh1994@gmail.com>
2020-06-14 21:09:38 +0530

    first commit

---
+++ 2.variables.py
@@ -0,0 +1,8 @@
+character_name = input("Enter Name: ")
+character_age = input("Enter Age: ")
+print("There once was a man named "+character_name+", ")
+print("he was "+character_age+" years old. ")
+
+character_name = "Mike"
+print("He really liked the name "+character_name+", ")
+print("but didn't like being "+character_age+".")
